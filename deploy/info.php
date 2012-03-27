fbab4824d700c377c50a301ecf3bc713094213e0
Ben Chambers <bchambers@clearfoundation.com>
2012-03-27 17:56:04 +0000

    - Bumped version to 1.0.6

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'registration';
-$app['version'] = '1.0.5';
+$app['version'] = '1.0.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
--- packaging/app-registration.spec
+++ packaging/app-registration.spec
@@ -1,7 +1,7 @@
 
 Name: app-registration
 Epoch: 1
-Version: 1.0.5
+Version: 1.0.6
 Release: 1%{dist}
 Summary: System Registration
 License: Proprietary
