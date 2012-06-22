95e3fe47af2b5135c4afe20f5dfa02832aceff5a
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-06-22 15:39:20 +0000

    - Prep for 6.3.0 Beta 1

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'registration';
-$app['version'] = '1.1.3';
+$app['version'] = '1.2.2';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
