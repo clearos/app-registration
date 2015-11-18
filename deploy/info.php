5397fafd92ed3e3aac41f4b6a9a9eedda1cf4e15
Benjamin Chambers <bchambers@clearfoundation.com>
2015-11-18 10:04:44 -0500

    Ensure data is encoded properly in ajax calls [Tracker #2201]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'registration';
-$app['version'] = '2.1.16';
+$app['version'] = '2.1.17';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
